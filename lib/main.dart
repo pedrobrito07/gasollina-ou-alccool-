95bb9cbc24366a031a3745b99eb7b4328af164a7
pedrobrito07 <61784441+pedrobrito07@users.noreply.github.com>
2022-05-02 21:06:35 -0300

    Primeira Versão

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:gasolina/widgets/logo.widgets.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alcool ou Gasolina',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: ListView(
+        children: <Widget>[
+          Logo(),
+        ],
+      ),
+    );
+  }
+}
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\pedrob\\Desktop\\gasolina\\gasolina" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\pedrob\\Desktop\\gasolina\\gasolina"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\pedrob\\Desktop\\gasolina\\gasolina\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\pedrob\\Desktop\\gasolina\\gasolina"
+  "FLUTTER_TARGET=C:\\Users\\pedrob\\Desktop\\gasolina\\gasolina\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\pedrob\\Desktop\\gasolina\\gasolina\\.dart_tool\\package_config.json"
+)
